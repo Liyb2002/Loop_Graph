3d976af2d699787f8ee5b0d6cb56374d3f196b95
liyb2002 <williamliyuanbo@gmail.com>
2025-07-29 20:30:52 -0700

    clean up

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,15 +4,15 @@ import Preprocessing.process_felix_dataset
 import Preprocessing.perturb_synthetic
 import Preprocessing.generate_dataset_baseline
 
-target = 'small'
+target = 'human'
 def run():
     
-    # d_generator = Preprocessing.perturb_synthetic.perturbation_dataset_loader()
+    d_generator = Preprocessing.perturb_synthetic.perturbation_dataset_loader()
 
     # d_generator = Preprocessing.perturb_stroke_cloud.perturbation_dataset_loader(target)
     # d_generator = Preprocessing.process_felix_dataset.cad2sketch_dataset_loader(target)
 
-    cad2sketch_generator = Preprocessing.generate_dataset_baseline.dataset_generator()
+    # cad2sketch_generator = Preprocessing.generate_dataset_baseline.dataset_generator()
 
 
 
